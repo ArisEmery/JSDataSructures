bb4735a59f8cc4a7d5faa38b3c06b81d468b1aa7
Aris <aris.emery@gmail.com>
2019-06-08 00:11:37 -0600

    added reverse method to linked list

--- LinkedList.js
+++ LinkedList.js
@@ -76,6 +76,27 @@ class LinkedList {
   }
 }
 
+    reverse() {
+      if (!this.head.next) {
+        return this.head;
+      }
+      let first = this.head;
+      this.tail = this.head;
+      let second = first.next;
+  
+      while(second) {
+        const temp = second.next;
+        second.next = first;
+        first = second;
+        second = temp;
+      }
+  
+      this.head.next = null;
+      this.head = first;
+      return this.printList();
+    }
+}
+
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);myLinkedList.prepend(1);
